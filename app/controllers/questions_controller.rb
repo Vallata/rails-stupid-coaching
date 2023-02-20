c220bb305b886493c185a45a6d1a18b863c61e33
Alizee Vallat <alizee.v@live.fr>
2023-02-20 14:14:46 +0100

    first rail app

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question] == 'i am going to work'
+      @answer = 'Great!'
+    elsif params[:question].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+# Si le message est I am going to work, le coach répondra Great!.
+# Si le message contient un point d’interrogation ? à la fin, le coach répondra Silly question, get dressed and go to work!.
+# Sinon, le coach répondra I don't care, get dressed and go to work!. %>
